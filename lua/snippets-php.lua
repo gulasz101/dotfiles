01b2e8d18b2ee0d1dd701c34efc1812a5c296300
Wojciech Gula <wojciechpiotr.gula@metro-markets.de>
2021-11-18 00:29:44 +0100

    First lsp php snipped for luasnip.

--- lua/snippets-php.lua
+++ lua/snippets-php.lua
@@ -9,17 +9,30 @@ local c = ls.choice_node
 local d = ls.dynamic_node
 local events = require("luasnip.util.events")
 
+--ls.snippets = {
+--    php = {
+--        s("trigger", {
+--            t({"", "After expanding, the cursor is here ->"}), i(1),
+--            t({"After jumping forward once, cursor is here ->"}), i(2),
+--            t({"", "After jumping once more, the snippet is exited there ->"}), i(0),
+--        })
+--    }
+--}
 ls.snippets = {
     php = {
-        s("trigger", {
-            t({"", "After expanding, the cursor is here ->"}), i(1),
-            t({"After jumping forward once, cursor is here ->"}), i(2),
-            t({"", "After jumping once more, the snippet is exited there ->"}), i(0),
-        })
+        ls.parser.parse_snippet(
+            "nci",
+            "<?php\
+\
+declare(strict_types=1);\
+\
+namespace ${1:Yours\\App\\Namespace};\
+\
+class ${TM_FILENAME_BASE} implements ${2:Interface}\
+{\
+\
+}\
+"
+        ),
     }
 }
-
-ls.parser.parse_snippet({trig = "phpnc"}, "<?php
-class ${TM_FILENAME} \n
-\n
-}")
