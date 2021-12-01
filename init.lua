09a2447551fdab940bc1de42dcdaac56c39a4930
Wojciech Gula <wojciechpiotr.gula@metro-markets.de>
2021-12-01 23:37:54 +0100

    Setting up split on going to definition with lsp.

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ require('packer-config') -- plugin manager
 require('lsp-config') -- base lsp conf
 require('cmp-config') -- base cmp conf
 
--- require('lsp-callbacks')
+require('lsp-handlers')
 -- require('snippets')
 
 require('luasnip-config')
