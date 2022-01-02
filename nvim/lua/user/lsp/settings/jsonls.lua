8f20c61857a5f3cb6ca369dc15f5cccde1f3dada
Wojciech Gula <wojciechpiotr.gula@metro-markets.de>
2022-01-02 14:09:50 +0100

    Adding support for package.json and composer.json. Configuring jsonls lsp server.

---
+++ nvim/lua/user/lsp/settings/jsonls.lua
@@ -0,0 +1,34 @@
+local default_schemas = nil
+local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
+if status_ok then default_schemas = jsonls_settings.get_default_schemas() end
+
+local schemas = {
+    {
+        description = "Composer configuration file",
+        fileMatch = {"composer.json"},
+        url = "https://raw.githubusercontent.com/composer/composer/master/res/composer-schema.json"
+    }, {description = "NPM configuration file", fileMatch = {"package.json"}, url = "https://json.schemastore.org/package.json"}
+}
+
+local function extend(tab1, tab2)
+    for _, value in ipairs(tab2) do table.insert(tab1, value) end
+    return tab1
+end
+
+local extended_schemas = extend(schemas, default_schemas)
+
+local opts = {
+    settings = {json = {schemas = extended_schemas}},
+    setup = {
+        commands = {
+            Format = {
+                function()
+                    vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line "$", 0})
+                end
+            }
+        },
+        capabilities = { completion = { completionItem = { snippetSupport = true}}},
+    }
+}
+
+return opts
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -59,7 +59,7 @@ return packer.startup(function(use)
     -- LSP
     use "neovim/nvim-lspconfig" -- enable LSP
     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-
+    use "tamago324/nlsp-settings.nvim"
     -- Telescope
     use "nvim-telescope/telescope.nvim"
     -- use "BurntSushi/ripgrep"
