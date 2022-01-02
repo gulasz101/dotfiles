fbac73e40be6ee25371354cf196f6bb952998dd8
Wojciech Gula <wojciechpiotr.gula@metro-markets.de>
2022-01-02 14:56:14 +0100

    Installing yaml-language-server with support for docker-compose files.

---
+++ nvim/lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,7 @@
+local opts = {
+    settings = {
+        yaml = {schemas = {["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose*yml"}}
+    }
+}
+
+return opts
